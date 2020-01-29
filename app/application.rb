438f654b98ecea9c991edaa516ef8bee3d37ab6e
Jonathan Ghandforoush <jonnyghan@gmail.com>
2020-01-29 00:32:19 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -14,8 +14,9 @@ class Application
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
-    elsif 
-    
+    elsif req.path.match(/cart/)
+    if @@cart.empty?
+      resp.write "Your cart is empty"
     
     else
       resp.write "Path Not Found"
